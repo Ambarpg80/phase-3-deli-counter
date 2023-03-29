b254af83153871f16f8ad498b4762ae297dc5432
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-03-29 12:40:56 -0400

    commit tree 5a948bfb16ba24450686f271079b516512220774

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+require 'pry'
+
+def line(customer_line)
+   if (customer_line.length > 0)
+        people_in_line = customer_line.map.with_index {|customer, index| "#{index += 1}. #{customer}"}
+        puts "The line is currently: #{people_in_line.join(" ")}" 
+    else
+       puts "The line is currently empty." 
+    end
+end
+
+
+def take_a_number(customer_line, customer)
+    if (customer_line.length >= 0)
+        customer_line << customer
+        puts "Welcome, #{customer}. You are number #{customer_line.length} in line."
+  end
+end
+
+def now_serving(customer_line)
+    if (customer_line.length == 0)
+       puts "There is nobody waiting to be served!"
+    else 
+        next_customer = customer_line.find do |customer| 
+            puts "Currently serving #{customer}."
+            customer_line.shift
+        end
+    end
+end
